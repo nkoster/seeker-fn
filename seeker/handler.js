fd94bab160f3a4840f4bffa3fa56befb1ff06a83
Niels Koster <niels.koster@portavita.nl>
2021-04-09 12:22:21 +0200

    All code inside module.exports + attempt to get rid of the query cancel error: did not work

--- seeker/handler.js
+++ seeker/handler.js
@@ -1,58 +1,57 @@
 'use strict'
 
-const DEBUG = true
-const DEVDELAY = 0
-const fs = require('fs')
-
-const config = {
-  database: process.env.PGDATABASE || 'postgres',
-  user: process.env.PGUSER || 'postgres',
-  host: process.env.PGHOST || 'db.fhirstation.net',
-  port: process.env.PGPORT || '5432',
-  ssl: {
-    rejectUnauthorized: false,
-    ca: fs.readFileSync('/home/app/function/tls/root.crt').toString(),
-    key: fs.readFileSync('/home/app/function/tls/client_postgres.key').toString(),
-    cert: fs.readFileSync('/home/app/function/tls/client_postgres.crt').toString()
-  }
-}
-
-const { Pool } = require('pg')
-const clientPool = new Pool(config)
-const pidKillerPool = new Pool(config)
-
-const LIMIT = process.env.SQLLIMIT || 51
-
-const sqlSelectQuery = queryId => {
-  return `
-SELECT kafka_topic, kafka_partition, kafka_offset, identifier_type, identifier_value
-/*${queryId}*/
-FROM dist_identifier_20210408
--- FROM identifier_20210311
-WHERE ($1 = '' OR kafka_topic ilike $1)
-AND   ($2 = '' OR identifier_type ilike $2)
-AND   ($3 = '' OR identifier_value ilike $3)
-AND   RIGHT('0000000000' || CAST(kafka_offset AS VARCHAR(10)), 10) like $4
-ORDER BY kafka_offset DESC
-LIMIT ${LIMIT}
-`
-}
-
-const sqlKillQuery = queryId => {
-  return `
-WITH pids AS (
-  /*notthisone*/
-  SELECT pid
-  FROM   pg_stat_activity
-  WHERE  query LIKE '%/*${queryId}*/%'
-  AND    query NOT LIKE '%/*notthisone*/%'
-  AND    state='active'
-)
-SELECT pg_cancel_backend(pid) FROM pids;
-`
-}
-
 module.exports = async (event, context) => {
+
+  const DEBUG = false
+  const fs = require('fs')
+  
+  const config = {
+    database: process.env.PGDATABASE || 'postgres',
+    user: process.env.PGUSER || 'postgres',
+    host: process.env.PGHOST || 'db.fhirstation.net',
+    port: process.env.PGPORT || '5432',
+    ssl: {
+      rejectUnauthorized: false,
+      ca: fs.readFileSync('/home/app/function/tls/root.crt').toString(),
+      key: fs.readFileSync('/home/app/function/tls/client_postgres.key').toString(),
+      cert: fs.readFileSync('/home/app/function/tls/client_postgres.crt').toString()
+    }
+  }
+  
+  const { Pool } = require('pg')
+  const clientPool = new Pool(config)
+  const pidKillerPool = new Pool(config)
+  
+  const LIMIT = process.env.SQLLIMIT || 51
+  
+  const sqlSelectQuery = queryId => {
+    return `
+  SELECT kafka_topic, kafka_partition, kafka_offset, identifier_type, identifier_value
+  /*${queryId}*/
+  FROM dist_identifier_20210408
+  -- FROM identifier_20210311
+  WHERE ($1 = '' OR kafka_topic ilike $1)
+  AND   ($2 = '' OR identifier_type ilike $2)
+  AND   ($3 = '' OR identifier_value ilike $3)
+  AND   RIGHT('0000000000' || CAST(kafka_offset AS VARCHAR(10)), 10) like $4
+  ORDER BY kafka_offset DESC
+  LIMIT ${LIMIT}
+  `
+  }
+  
+  const sqlKillQuery = queryId => {
+    return `
+  WITH pids AS (
+    /*notthisone*/
+    SELECT pid
+    FROM   pg_stat_activity
+    WHERE  query LIKE '%/*${queryId}*/%'
+    AND    query NOT LIKE '%/*notthisone*/%'
+    AND    state='active'
+  )
+  SELECT pg_cancel_backend(pid) FROM pids;
+  `
+  }
 
   let data
 
@@ -84,13 +83,15 @@ module.exports = async (event, context) => {
   }
 
   data = await new Promise((resolve, reject) => {
+    let result
     try {
-      resolve(client.query(query))
+      result = client.query(query)
       DEBUG && console.log('rows:', data.rows.length)
     } catch (err) {
-      reject(err)
+      reject({ rows: [] })
       DEBUG && console.log(err.message)
     } finally {
+      resolve(result)
       client.release()
     }    
   })
